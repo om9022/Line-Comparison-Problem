297fe8517905d204795c0dbc495d99bd56f668c5
om9022 <omkarchikane607@gmail.com>
2021-06-21 20:00:54 +0530

    Added: Welcome to Line Comparison Computation

---
+++ src/main/java/com/Line_Comparison_Problem/App.java
@@ -0,0 +1,10 @@
+package com.Line_Comparison_Problem;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation" );
+    }
+}
